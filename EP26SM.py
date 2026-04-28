a7a1e3d5301d66283d7f7c562d322dead97cf63e
MK <99416359+MK316@users.noreply.github.com>
2026-04-28 18:12:22 +0900

    Update EP26SM.py

--- EP26SM.py
+++ EP26SM.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Score Checker", layout="wide")
 
-st.title("📘 S26 Midterm Score Checker")
+st.title("🍰 S26 Midterm Score Checker")
 st.caption("You need your email address and passcode you'd provided for the exam.")
 
 DATASETS = {
